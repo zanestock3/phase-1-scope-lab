5b2904dd025ce197d693a46a014c1c748d3e1f1e
Zane Stock <zanestock3@gmail.com>
2022-04-07 18:03:19 -0500

    commit tree ddc7d0e0b6322abebf8ce49296337659e66eba96

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer= "not bob";
+}
+
+function overwriteBestCustomer(bob) {
+    bestCustomer = bob;
+}
+const leastFavoriteCustomer = "maybe bob";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bill"
+}
